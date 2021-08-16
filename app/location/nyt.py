48cc9d208874d3af00b3339ab2a62610c3eaf592
Zhixu Zhao <zxzilovecoding@gmail.com>
2021-08-15 22:39:57 -0400

    Creational Pattern: Builder pattern applied

--- app/location/nyt.py
+++ app/location/nyt.py
@@ -1,5 +1,6 @@
 """app.locations.nyt.py"""
-from . import TimelinedLocation
+from Commit1 import BaseInfo, Director, TimelinedLocation_Builder
+from . import BuildTimeLinedLocation, TimelinedLocation, Director, BaseLocation, Locationinfo, CaseNumbers
 
 
 class NYTLocation(TimelinedLocation):
@@ -9,10 +10,17 @@ class NYTLocation(TimelinedLocation):
 
     # pylint: disable=too-many-arguments,redefined-builtin
     def __init__(self, id, state, county, coordinates, last_updated, timelines):
-        super().__init__(id, "US", state, coordinates, last_updated, timelines)
+        """ super().__init__(id, "US", state, coordinates, last_updated, timelines)
 
         self.state = state
-        self.county = county
+        self.county = county """
+
+        director = Director()
+        base = BaseInfo(id=id,last_updated=last_updated)
+        locationinfo = Locationinfo(country="US", province=state, coordinates=coordinates)
+        builder = BuildTimeLinedLocation(baseinfo=base, geoinfo=locationinfo, timelines=timelines)
+        director.set_builder(builder)
+        nyt = director.build_location
 
     def serialize(self, timelines=False):  # pylint: disable=arguments-differ,unused-argument
         """
